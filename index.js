f90c16a845a2c710db011bbf1812caccb903390c
Pereira <ricardo.jose.almeida.pereira@ubi.pt>
2022-07-24 10:54:59 +0100

    $

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
+const Discord = require("discord.js");
 const {token} = require("./tkn.json");
 const prefix = '-';
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.on('ready', () =>{
-    console.log("Ready for Work!")
+const bot = new Discord.Client({
+    allowedMentions: {
+        parse: ['users', 'roles'],
+        repliedUser: true,
+    },
+    intents: 3276797
 });
 
-client.login(token);
+bot.on('ready', () => {
+    console.log("Ready for Work!") //console response to the bot being online
+    bot.user.setActivity("with ur Mom");
+});
+
+bot.on('messageCreate', msg =>{
+    //    CHECKING MGS    //
+    if(msg.author.bot) return; //avoid bot loop messages and commands
+
+    if(msg.content === prefix + "Hello" || msg.content === prefix + "hello"){
+        msg.reply ("Hello Big Fella");
+    }else if (msg.content === prefix + "Server" || msg.content === prefix + "server"){
+        msg.reply (`This Server have ${bot.guilds.cache.size} members in this moment`);
+    }
+});
+
+bot.login(token); //take the token of the bot and put it online
