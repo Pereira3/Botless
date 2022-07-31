60931cf2e32f3d1a80efb01b24bce30b3fb28642
Pereira <ricardo.jose.almeida.pereira@ubi.pt>
2022-07-31 22:44:17 +0100

    upt SlashEmbed

---
+++ Commands/Public/SlashEmbed.js
@@ -0,0 +1,22 @@
+const {MessageEmbed, MembershipScreeningFieldType, Message} = require("discord.js");
+const moment = require('moment');
+
+module.exports = {
+    name: "UserInfo",
+    aliase: ['ui'],
+    description: "Display the mentioned member's information or the command user's",
+    execute(message, args, commandName, client, Discord){
+        const Target = message.mentions.users.first() || message.author;
+        const Member = message.guild.members.cache.get(Target.id);
+
+        const Response = new MessageEmbed()
+        .setAuthor(`${Target.username}`, Target.displayAvatarURL({dynamic: true}))
+        .setThumbnail(Target.displayAvatarURL({dynamic: true}))
+        .setColor(0xffffff)
+        .addField("UserID", `${Target.id}`, false)
+        .addField("Roles", `${Member.roles.cache.map(r=>r).join(' ').replace("@everyone", " ")}`)
+        .addField("Server Member Since:", `${moment(Member.joinedAt).format('MMMM Do YYYY, hh:mm:ss a')}\n**-** ${moment(Member.joinedAt).startOf('day').fromNow()}`)
+        .addField("Discord User Since:", `${moment(Target.createdAt).format('MMMM Do YYYY, hh:mm:ss a')}\n**-** ${moment(Target.createdAt).startOf('day').fromNow()}`)
+        message.reply({embeds: [Response]});
+    }
+}
